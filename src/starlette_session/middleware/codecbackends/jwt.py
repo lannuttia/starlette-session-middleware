cc460c9a3ebc75f7f1374f2499ec2075bed525e3
Anthony T. Lannutti <alannutt@redhat.com>
2023-08-27 00:28:41 -0500

    Added support for JWT codec backend

---
+++ src/starlette_session/middleware/codecbackends/jwt.py
@@ -0,0 +1,28 @@
+import typing
+
+import jwt
+from jwt.exceptions import InvalidTokenError
+
+from starlette.datastructures import Secret
+
+from starlette_session.middleware.codecbackends.errors import DecodeError
+from starlette_session.middleware.codecbackends.base import CodecBackendInterface
+
+
+class JwtBackend(CodecBackendInterface):
+    def __init__(
+        self,
+        key: typing.Union[str, Secret],
+        algorithm: str,
+    ):
+        self.key = key
+        self.algorithm = algorithm
+
+    def decode(self, value: str) -> typing.Any:
+        try:
+            return jwt.decode(value, key=self.key, algorithms=[self.algorithm])
+        except InvalidTokenError as ex:
+            raise DecodeError("Failed to decode value") from ex
+
+    def encode(self, data: dict[str, typing.Any]) -> str:
+        return str(jwt.encode(data, key=self.key, algorithm=self.algorithm))
--- tests/middleware/test_session.py
+++ tests/middleware/test_session.py
@@ -1,4 +1,6 @@
 import re
+from datetime import datetime, timedelta
+from time import sleep
 
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
@@ -7,6 +9,7 @@ from starlette.routing import Mount, Route
 from starlette.testclient import TestClient
 
 from starlette_session.middleware.session import SessionMiddleware
+from starlette_session.middleware.codecbackends.jwt import JwtBackend
 from starlette_session.middleware.codecbackends.signer import SignerBackend
 from starlette_session.middleware.storagebackends.cookie import CookieBackend
 from starlette_session.middleware.authorizationbackends.cookie import (
@@ -27,6 +30,225 @@ async def update_session(request):
 async def clear_session(request):
     request.session.clear()
     return JSONResponse({"session": request.session})
+
+
+class TestJwtBackendWithCookieAuthorization:
+    def test_session(self, test_client_factory):
+        app = Starlette(
+            routes=[
+                Route("/view_session", endpoint=view_session),
+                Route("/update_session", endpoint=update_session, methods=["POST"]),
+                Route("/clear_session", endpoint=clear_session, methods=["POST"]),
+            ],
+            middleware=[
+                Middleware(
+                    SessionMiddleware,
+                    codec_backend=JwtBackend(key="example", algorithm="HS256"),
+                    storage_backend=CookieBackend(),
+                    authorization_backend=CookieAuthorizationBackend(),
+                )
+            ],
+        )
+        client = test_client_factory(app)
+
+        response = client.get("/view_session")
+        assert response.json() == {"session": {}}
+
+        now = datetime.now()
+        exp = datetime.timestamp(now + timedelta(days=14))
+        response = client.post("/update_session", json={"exp": exp, "some": "data"})
+        assert response.json() == {"session": {"exp": exp, "some": "data"}}
+
+        # check cookie max-age
+        set_cookie = response.headers["set-cookie"]
+        max_age_matches = re.search(r"; Max-Age=([0-9]+);", set_cookie)
+        assert max_age_matches is not None
+        assert int(max_age_matches[1]) == int(14 * 24 * 3600)
+
+        response = client.get("/view_session")
+        assert response.json() == {"session": {"exp": exp, "some": "data"}}
+
+        response = client.post("/clear_session")
+        assert response.json() == {"session": {}}
+
+        response = client.get("/view_session")
+        assert response.json() == {"session": {}}
+
+    def test_session_expires(self, test_client_factory):
+        app = Starlette(
+            routes=[
+                Route("/view_session", endpoint=view_session),
+                Route("/update_session", endpoint=update_session, methods=["POST"]),
+            ],
+            middleware=[
+                Middleware(
+                    SessionMiddleware,
+                    codec_backend=JwtBackend(key="example", algorithm="HS256"),
+                    storage_backend=CookieBackend(),
+                    authorization_backend=CookieAuthorizationBackend(),
+                )
+            ],
+        )
+        client = test_client_factory(app)
+
+        now = datetime.now()
+        exp = datetime.timestamp(now + timedelta(seconds=1))
+        response = client.post("/update_session", json={"exp": exp, "some": "data"})
+        assert response.json() == {"session": {"exp": exp, "some": "data"}}
+        response = client.get("/view_session")
+        assert response.json() == {"session": {"exp": exp, "some": "data"}}
+        sleep(1)
+        response = client.get("/view_session")
+        assert response.json() == {"session": {}}
+
+    def test_session_future_nbf(self, test_client_factory):
+        app = Starlette(
+            routes=[
+                Route("/view_session", endpoint=view_session),
+                Route("/update_session", endpoint=update_session, methods=["POST"]),
+            ],
+            middleware=[
+                Middleware(
+                    SessionMiddleware,
+                    codec_backend=JwtBackend(key="example", algorithm="HS256"),
+                    storage_backend=CookieBackend(),
+                    authorization_backend=CookieAuthorizationBackend(),
+                )
+            ],
+        )
+        client = test_client_factory(app)
+
+        now = datetime.now()
+        nbf = datetime.timestamp(now + timedelta(seconds=1))
+        response = client.post("/update_session", json={"nbf": nbf, "some": "data"})
+        assert response.json() == {"session": {"nbf": nbf, "some": "data"}}
+        response = client.get("/view_session")
+        assert response.json() == {"session": {}}
+        sleep(1)
+        response = client.post("/update_session", json={"nbf": nbf, "some": "data"})
+        assert response.json() == {"session": {"nbf": nbf, "some": "data"}}
+        response = client.get("/view_session")
+        assert response.json() == {"session": {"nbf": nbf, "some": "data"}}
+
+    def test_secure_session(self, test_client_factory):
+        app = Starlette(
+            routes=[
+                Route("/view_session", endpoint=view_session),
+                Route("/update_session", endpoint=update_session, methods=["POST"]),
+                Route("/clear_session", endpoint=clear_session, methods=["POST"]),
+            ],
+            middleware=[
+                Middleware(
+                    SessionMiddleware,
+                    codec_backend=JwtBackend(key="example", algorithm="HS256"),
+                    storage_backend=CookieBackend(https_only=True),
+                    authorization_backend=CookieAuthorizationBackend(),
+                )
+            ],
+        )
+        secure_client = test_client_factory(app, base_url="https://testserver")
+        unsecure_client = test_client_factory(app, base_url="http://testserver")
+
+        response = unsecure_client.get("/view_session")
+        assert response.json() == {"session": {}}
+
+        response = unsecure_client.post("/update_session", json={"some": "data"})
+        assert response.json() == {"session": {"some": "data"}}
+
+        response = unsecure_client.get("/view_session")
+        assert response.json() == {"session": {}}
+
+        response = secure_client.get("/view_session")
+        assert response.json() == {"session": {}}
+
+        response = secure_client.post("/update_session", json={"some": "data"})
+        assert response.json() == {"session": {"some": "data"}}
+
+        response = secure_client.get("/view_session")
+        assert response.json() == {"session": {"some": "data"}}
+
+        response = secure_client.post("/clear_session")
+        assert response.json() == {"session": {}}
+
+        response = secure_client.get("/view_session")
+        assert response.json() == {"session": {}}
+
+    def test_session_cookie_subpath(self, test_client_factory):
+        second_app = Starlette(
+            routes=[
+                Route("/update_session", endpoint=update_session, methods=["POST"]),
+            ],
+            middleware=[
+                Middleware(
+                    SessionMiddleware,
+                    codec_backend=JwtBackend(key="example", algorithm="HS256"),
+                    storage_backend=CookieBackend(path="/second_app"),
+                    authorization_backend=CookieAuthorizationBackend(),
+                )
+            ],
+        )
+        app = Starlette(routes=[Mount("/second_app", app=second_app)])
+        client = test_client_factory(app, base_url="http://testserver/second_app")
+        response = client.post("/second_app/update_session", json={"some": "data"})
+        assert response.status_code == 200
+        cookie = response.headers["set-cookie"]
+        cookie_path_match = re.search(r"; path=(\S+);", cookie)
+        assert cookie_path_match is not None
+        cookie_path = cookie_path_match.groups()[0]
+        assert cookie_path == "/second_app"
+
+    def test_invalid_session_cookie(self, test_client_factory):
+        app = Starlette(
+            routes=[
+                Route("/view_session", endpoint=view_session),
+                Route("/update_session", endpoint=update_session, methods=["POST"]),
+            ],
+            middleware=[
+                Middleware(
+                    SessionMiddleware,
+                    codec_backend=JwtBackend(key="example", algorithm="HS256"),
+                    storage_backend=CookieBackend(),
+                    authorization_backend=CookieAuthorizationBackend(),
+                )
+            ],
+        )
+        client = test_client_factory(app)
+
+        response = client.post("/update_session", json={"some": "data"})
+        assert response.json() == {"session": {"some": "data"}}
+
+        # we expect it to not raise an exception if we provide a bogus session cookie
+        client = test_client_factory(app, cookies={"session": "invalid"})
+        response = client.get("/view_session")
+        assert response.json() == {"session": {}}
+
+    def test_session_cookie(self, test_client_factory):
+        app = Starlette(
+            routes=[
+                Route("/view_session", endpoint=view_session),
+                Route("/update_session", endpoint=update_session, methods=["POST"]),
+            ],
+            middleware=[
+                Middleware(
+                    SessionMiddleware,
+                    codec_backend=JwtBackend(key="example", algorithm="HS256"),
+                    storage_backend=CookieBackend(max_age=None),
+                    authorization_backend=CookieAuthorizationBackend(),
+                )
+            ],
+        )
+        client: TestClient = test_client_factory(app)
+
+        response = client.post("/update_session", json={"some": "data"})
+        assert response.json() == {"session": {"some": "data"}}
+
+        # check cookie max-age
+        set_cookie = response.headers["set-cookie"]
+        assert "Max-Age" not in set_cookie
+
+        client.cookies.delete("session")
+        response = client.get("/view_session")
+        assert response.json() == {"session": {}}
 
 
 class TestSignerBackendWithCookieAuthorization:
