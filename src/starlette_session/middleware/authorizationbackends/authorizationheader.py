adae0d51baa18307c5b4712f6872832030d97f04
Anthony T. Lannutti <alannutt@redhat.com>
2023-08-27 20:00:10 -0500

    Add support for Authorization header authorization backend

---
+++ src/starlette_session/middleware/authorizationbackends/authorizationheader.py
@@ -0,0 +1,23 @@
+import typing
+
+
+from starlette.requests import HTTPConnection
+
+
+from starlette_session.middleware.authorizationbackends.base import (
+    AuthorizationBackendInterface,
+)
+
+
+class AuthorizationHeaderAuthorizationBackend(AuthorizationBackendInterface):
+    def __init__(self, type: str = "Bearer"):
+        self.type = type
+
+    def get_token(self, connection: HTTPConnection) -> typing.Union[str, None]:
+        header = "Authorization"
+        expected_value_prefix = f"{self.type} "
+        if header not in connection.headers or not connection.headers[
+            header
+        ].startswith(expected_value_prefix):
+            return None
+        return str(connection.headers[header].split(expected_value_prefix, 1)[1])
