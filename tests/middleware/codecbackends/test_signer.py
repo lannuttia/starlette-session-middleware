2bea4229537935446a6d192ab499690ac024f863
Anthony T. Lannutti <alannutt@redhat.com>
2023-08-29 10:06:35 -0500

    Added tests for signer backend

---
+++ tests/middleware/codecbackends/test_signer.py
@@ -0,0 +1,27 @@
+import pytest
+from time import sleep
+
+from starlette_session.middleware.codecbackends.signer import SignerBackend
+from starlette_session.middleware.codecbackends.errors import DecodeError
+from starlette_session.middleware.codecbackends import CodecBackendInterface
+
+
+class TestSignerBackend:
+    def test_is_expected_subclass(self):
+        signer_backend = SignerBackend(key="superdupersecret")
+        assert isinstance(signer_backend, CodecBackendInterface) is True
+
+    def test_successful_decode(self):
+        signer_backend = SignerBackend(key="superdupersecret")
+        claims = {"test": "data"}
+        token = signer_backend.encode(claims)
+        assert signer_backend.decode(token) == claims
+
+    def test_decode_after_max_age(self):
+        signer_backend = SignerBackend(key="superdupersecret", max_age=0)
+        claims = {"test": "data"}
+        token = signer_backend.encode(claims)
+        assert signer_backend.decode(token) == claims
+        sleep(1)
+        with pytest.raises(DecodeError):
+            signer_backend.decode(token)
