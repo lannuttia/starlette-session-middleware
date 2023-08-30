a2fe2233b3547b4aea4eaeec6c48f1bd5f788b6a
Anthony T. Lannutti <alannutt@redhat.com>
2023-08-30 09:19:11 -0500

    Added docstrings for JwtCodecBackend

--- src/starlette_session/middleware/codecbackends/jwt.py
+++ src/starlette_session/middleware/codecbackends/jwt.py
@@ -11,19 +11,66 @@ from starlette_session.middleware.codecbackends import (
 
 
 class JwtCodecBackend(CodecBackendInterface):
+    """
+    A codec backend for encoding and decoding JWTs.
+
+    Methods
+    -------
+    decode(value: str)
+        Decodes the provided JWT.
+    encode(value: dict[str, Any])
+        Encodes the provided claims as a JWT.
+    """
+
     def __init__(
         self,
         key: typing.Union[str, Secret],
         algorithm: str,
     ):
-        self.key = key
-        self.algorithm = algorithm
+        """
+        Constructs an instance of a JwtCodecBackend.
+
+        Parameters
+        ----------
+        key: Union[str, Secret]
+            The key used to sign and verify the integrety of the JWT.
+        algorithm: str
+            The algorithm used to encode and decode the JWT.
+        """
+        self._key = key
+        self._algorithm = algorithm
 
     def decode(self, value: str) -> typing.Any:
+        """
+        Verifies the validity, integrity and decodes the provided JWT.
+
+        raises DecodeError if the provided JWT is not valid or has been tampered with.
+
+        Parameters
+        ----------
+        value: str
+            The JWT that is to be decoded
+
+        Returns
+        -------
+        dict[str, Any]
+        """
         try:
-            return jwt.decode(value, key=self.key, algorithms=[self.algorithm])
+            return jwt.decode(value, key=self._key, algorithms=[self._algorithm])
         except InvalidTokenError as ex:
             raise DecodeError("Failed to decode value") from ex
 
     def encode(self, data: dict[str, typing.Any]) -> str:
-        return str(jwt.encode(data, key=self.key, algorithm=self.algorithm))
+        """
+        Encodes the provided claims as a JWT.
+
+        Parameters
+        ----------
+        data: dict[str, Any]
+            The claims that are to be encoded as a JWT.
+
+        Returns
+        -------
+        str
+        """
+        return str(jwt.encode(data, key=self._key, algorithm=self._algorithm))
