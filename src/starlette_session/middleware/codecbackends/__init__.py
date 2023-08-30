e5eb213394f0a8a17450747db861e97c7386680d
Anthony T. Lannutti <alannutt@redhat.com>
2023-08-30 09:09:09 -0500

    Add docstring to CodecBackendInterface

--- src/starlette_session/middleware/codecbackends/__init__.py
+++ src/starlette_session/middleware/codecbackends/__init__.py
@@ -3,6 +3,17 @@ import typing
 
 
 class CodecBackendInterface(metaclass=abc.ABCMeta):
+    """
+    An interface that can be used to implement a codec backend.
+
+    Methods
+    -------
+    decode(value: str)
+        Decodes the provided string.
+    encode(value: dict[str, Any])
+        Encodes the provided dictionary.
+    """
+
     @classmethod
     def __subclasshook__(cls, __subclass: type) -> bool:
         return (
@@ -15,12 +26,32 @@ class CodecBackendInterface(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def decode(self, value: str) -> typing.Any:
+        """
+        Raises NotImplementedError
+
+        Parameters
+        ----------
+        value: str
+            The string to be decoded.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def encode(self, value: dict[str, typing.Any]) -> str:
+        """
+        Raises NotImplementedError
+
+        Parameters
+        ----------
+        value: dict[str, Any]
+            The value to be encoded.
+        """
         raise NotImplementedError
 
 
 class DecodeError(Exception):
+    """
+    An error to raise if decoding a token can not or should not happen.
+    """
+
     pass
