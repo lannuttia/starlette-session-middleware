01103dbfdb2d59dde9f4e0402ad0bf29fd26fff5
Anthony T. Lannutti <alannutt@redhat.com>
2024-01-18 15:42:27 -0600

    Added docstring for StorageBackendInterface

--- src/starlette_session/middleware/storagebackends/__init__.py
+++ src/starlette_session/middleware/storagebackends/__init__.py
@@ -4,6 +4,17 @@ from starlette.types import Message
 
 
 class StorageBackendInterface(metaclass=abc.ABCMeta):
+    """
+    An interface that can be used to implement a storage backend.
+
+    Methods
+    -------
+    persist(message: Message, data: str)
+        Persists the data.
+    clear(message: Message)
+        Clears the data.
+    """
+
     @classmethod
     def __subclasshook__(cls, __subclass: type) -> bool:
         return (
@@ -16,8 +27,26 @@ class StorageBackendInterface(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def persist(self, message: Message, data: str) -> None:
+        """
+        Raises NotImplementedError
+
+        Parameters
+        ----------
+        message: Message
+            The message to persist the data on.
+        data: str
+            The data to persist.
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def clear(self, message: Message) -> None:
+        """
+        Raises NotImplementedError
+
+        Parameters
+        ----------
+        message: Message
+            The message to clear the data from.
+        """
         raise NotImplementedError
