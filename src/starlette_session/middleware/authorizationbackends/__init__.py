11e22549649116a5d0a9ca9af61946f63e3135e0
Anthony T. Lannutti <alannutt@redhat.com>
2024-01-18 15:54:35 -0600

    Added docstring for AuthorizationBackendInterface

--- src/starlette_session/middleware/authorizationbackends/__init__.py
+++ src/starlette_session/middleware/authorizationbackends/__init__.py
@@ -5,10 +5,27 @@ from starlette.requests import HTTPConnection
 
 
 class AuthorizationBackendInterface(metaclass=abc.ABCMeta):
+    """
+    An interface that can be used to implement an authorization backend.
+
+    Methods
+    -------
+    get_token(connection: HTTPConnection)
+        gets the token.
+    """
+
     @classmethod
     def __subclasshook__(cls, __subclass: type) -> bool:
         return hasattr(__subclass, "get_token") and callable(__subclass.get_token)
 
     @abc.abstractmethod
     def get_token(self, connection: HTTPConnection) -> typing.Union[str, None]:
+        """
+        Raises NotImplementedError
+
+        Parameters
+        ----------
+        value: HTTPConnection
+            The connection that the token can be retrieved from.
+        """
         raise NotImplementedError
