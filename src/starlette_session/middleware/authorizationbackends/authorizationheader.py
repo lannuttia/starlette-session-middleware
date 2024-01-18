c3ab2a27f80ad389f57e82f4d080051e698b3239
Anthony T. Lannutti <alannutt@redhat.com>
2024-01-18 15:55:02 -0600

    Added docstring for AuthorizationHeaderAuthorizationBackend

--- src/starlette_session/middleware/authorizationbackends/authorizationheader.py
+++ src/starlette_session/middleware/authorizationbackends/authorizationheader.py
@@ -9,10 +9,39 @@ from starlette_session.middleware.authorizationbackends import (
 
 
 class AuthorizationHeaderAuthorizationBackend(AuthorizationBackendInterface):
+    """
+    An authorization backend for retrieving the token through the HTTP Authorization header.
+
+    Methods
+    -------
+    get_token(connection: HTTPConnection)
+        Retrieves the token from the HTTP Authorization header.
+    """
+
     def __init__(self, type: str = "Bearer"):
+        """
+        Constructs an instance of an AuthorizationHeaderAuthorizationBackend.
+
+        Parameters
+        ----------
+        type: str
+            The token type that is expected.
+        """
         self.pattern = re.compile(f"^{type.lower()} (.*)$", re.I)
 
     def get_token(self, connection: HTTPConnection) -> typing.Union[str, None]:
+        """
+        Retreives the token from the HTTP Authorization header.
+
+        Parameters
+        ----------
+        connection: HTTPConnection
+            The connection that the token will be retrieved from.
+
+        Returns
+        -------
+        Union[str, None]
+        """
         header = "Authorization"
         if header not in connection.headers:
             return None
